ed5f44bb2cf39a842aace17460d2380581620789
alpha <56209673+strunged@users.noreply.github.com>
2022-07-24 03:59:28 -0300

    #

---
+++ assets/js/enter.js
@@ -0,0 +1,15 @@
+document.getElementById('main').hidden = true;
+var vid = document.getElementById('video');
+
+document.addEventListener('click', function () {
+	document.getElementById('enter').hidden = true;
+	document.getElementById('main').hidden = false;
+
+	if (typeof window.orientation == 'undefined') { 
+		vid.play();
+	}
+});
+
+vid.onended = function () {
+	vid.play();
+};
